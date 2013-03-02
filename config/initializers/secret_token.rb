7f810be3a9a85acabda292e2944d5e404ed19345
maveral <maveral@maveral.megiteam.pl>
2013-03-02 16:54:45 +0100

    Next5

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Setty::Application.config.secret_token = 'cc3a0c9b1c894d780c6bdce9b4f3874485677776b38b77ce47a05c6a84886dd8c04b2004ba3083eb9cdd35c7b277c6055836581c71fdf7e091f32c4a0f851a7e'
+Setty::Application.config.secret_token = '547b831e337bec899d30f33d28a53c4e20e6039ecd702b212b27ba6299eab9af1a6534c0847985b21b9bebbeb83012c6db953eb4b8c512f30809f5e68b835deb'
