87c187678ec8079a130c6703db324a85904114ce
maveral <maveral@maveral.megiteam.pl>
2013-03-02 16:31:41 +0100

    Next3

--- config/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Setty::Application.config.secret_token = 'cc3a0c9b1c894d780c6bdce9b4f3874485677776b38b77ce47a05c6a84886dd8c04b2004ba3083eb9cdd35c7b277c6055836581c71fdf7e091f32c4a0f851a7e'
